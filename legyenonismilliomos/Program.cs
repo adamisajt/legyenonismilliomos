d10ca2e0ba1e587ed7ed383f1d390c43036c5e22
aczee <aczee@ADAM>
2025-05-09 10:57:04 +0200

    Add project files.

---
+++ legyenonismilliomos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace legyenonismilliomos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

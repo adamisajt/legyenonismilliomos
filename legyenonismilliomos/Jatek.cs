5e156cb600e4f79fb9f25c739979cf47860252da
aczee <aczee@ADAM>
2025-05-23 10:23:56 +0200

    ujrairt

--- legyenonismilliomos/Jatek.cs
+++ legyenonismilliomos/Jatek.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using legyenonismilliomos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -9,13 +10,62 @@ namespace legyenonismilliomos
 {
     internal class Jatek
     {
+        private int pont = 0;
+        private int[] nyeremenyek = [10000, 20000, 50000, 100000, 250000, 500000, 750000, 1000000, 1500000, 2000000, 5000000, 10000000, 15000000, 25000000, 50000000];
+
         static Random rnd = new Random();
 
-        public void SorkerdesGeneralas(List<Sorkerdes> sorkerdesek)
+        public int Pont { get => pont; }
+        public int[] Nyeremenyek { get => nyeremenyek; }
+
+        public bool SorkerdesGeneralas(List<Sorkerdes> sorkerdesek)
         {
             Sorkerdes sk = sorkerdesek[rnd.Next(0, sorkerdesek.Count - 1)];
 
             Console.WriteLine(sk.KerdesSzoveg);
+            Console.WriteLine($"\na) {sk.Valaszok[0]}\t\t\tb) {sk.Valaszok[1]}\nc) {sk.Valaszok[2]}\t\t\td) {sk.Valaszok[3]}");
+            Console.WriteLine("\nÍrja be a helyes válasz sorrendjét(pl: ABCD): ");
+            string valasz = Console.ReadLine().Trim().ToUpper();
+
+            if (valasz.Equals(sk.HelyesSorrend))
+            {
+                Console.WriteLine("\nHelyes válasz! továbbléphet az első kérdésre!");
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nHelytelen válasz. Helyes:\n{sk.HelyesSorrend}");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
+        }
+
+        public bool KerdesGeneralas(List<List<Kerdes>> kerdesek)
+        {
+            Kerdes k = kerdesek[pont][rnd.Next(0, kerdesek.Count - 1)];
+            Console.WriteLine("1. " + k.KerdesSzoveg);
+            Console.WriteLine($"\na) {k.Valaszok[0]}\t\t\tb) {k.Valaszok[1]}\nc) {k.Valaszok[2]}\t\t\td) {k.Valaszok[3]}");
+            Console.WriteLine("\nÍrja be a helyes válasz betűjelét (pl: A)");
+
+            string valasz = Console.ReadLine().Trim().ToUpper();
+            if (valasz.Equals(k.Helyes))
+            {
+                Console.WriteLine($"Helyes válasz! Továbbmehet a következő kérdésre. Eddig összegűjtött pénz: {nyeremenyek[pont]}");
+                pont++;
+                Console.ReadKey();
+                Console.Clear();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Helytelen válasz! Helyes: {k.Helyes}\nVége a játéknak");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
         }
     }
 }
--- legyenonismilliomos/Program.cs
+++ legyenonismilliomos/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using legyenonismilliomos;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -11,28 +12,38 @@ namespace legyenonismilliomos
             List<List<Kerdes>> kerdesek = KerdesOlvasas();
             List<Sorkerdes> sorkerdesek = SorkerdesOlvasas();
 
-            
+            Jatek jatek = new Jatek();
+
+            if (jatek.SorkerdesGeneralas(sorkerdesek))
+            {
+                bool fut = true;
+                while (jatek.Pont < 15 && fut)
+                {
+                    fut = jatek.KerdesGeneralas(kerdesek);
+                }
+            }
         }
 
         static List<Sorkerdes> SorkerdesOlvasas()
         {
-            List<Sorkerdes> sorkerdesek = new List<Sorkerdes>();
+            List<Sorkerdes> sorkerdesek = [];
 
-            using (StreamReader sr = new StreamReader("kerdes.txt"))
+            StreamReader sr = new StreamReader("sorkerdes.txt");
+            string[] sorok = sr.ReadToEnd().Split("\n");
+
+            foreach (string sor in sorok)
             {
-                string[] sorok = sr.ReadToEnd().Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (string sor in sorok)
+                if (sor.Equals(""))
                 {
-                    string[] reszek = sor.Split(';');
-
-                    
-                    List<string> valaszok = new List<string> { reszek[1], reszek[2], reszek[3], reszek[4] };
-
-                    Sorkerdes sk = new Sorkerdes(reszek[0], valaszok, reszek[5], reszek[6]);
-
-                    sorkerdesek.Add(sk);
+                    return sorkerdesek;
                 }
+                string[] reszek = sor.Split(";");
+
+                List<string> valaszok = [reszek[1], reszek[2], reszek[3], reszek[4]];
+
+                Sorkerdes sk = new Sorkerdes(reszek[0], valaszok, reszek[5], reszek[6]);
+
+                sorkerdesek.Add(sk);
             }
 
             return sorkerdesek;
@@ -40,47 +51,32 @@ namespace legyenonismilliomos
 
         static List<List<Kerdes>> KerdesOlvasas()
         {
-            List<List<Kerdes>> kerdesek = new List<List<Kerdes>>();
+            List<List<Kerdes>> kerdesek = [];
 
+           
             for (int i = 0; i < 15; i++)
             {
                 kerdesek.Add(new List<Kerdes>());
             }
 
-            using (StreamReader sr = new StreamReader("kerdes.txt"))
+            StreamReader sr = new StreamReader("kerdes.txt");
+            string[] sorok = sr.ReadToEnd().Split("\n");
+
+            foreach (string sor in sorok)
             {
-                string[] sorok = sr.ReadToEnd().Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (string sor in sorok)
+                if (sor.Equals(""))
                 {
-                    string[] reszek = sor.Split(';');
-
-                   
-                    List<string> valaszok = new List<string> { reszek[2], reszek[3], reszek[4], reszek[5] };
-
-                    int helyes = reszek[6] switch
-                    {
-                        "A" => 0,
-                        "B" => 1,
-                        "C" => 2,
-                        "D" => 3,
-                        _ => -1
-                    };
-
-                    if (helyes == -1)
-                    {
-                        
-                        continue;
-                    }
-
-                    int szint = int.Parse(reszek[0]) - 1;
-
-                    Kerdes k = new Kerdes(szint, reszek[1], valaszok, helyes, reszek[7]);
-
-                    kerdesek[szint].Add(k);
+                    return kerdesek;
                 }
-            }
+                string[] reszek = sor.Split(";");
 
+                List<string> valaszok = [reszek[2], reszek[3], reszek[4], reszek[5]];
+
+               
+                Kerdes k = new Kerdes(int.Parse(reszek[0]) - 1, reszek[1], valaszok, reszek[6], reszek[7]);
+
+                kerdesek[k.Szint].Add(k);
+            }
 
             return kerdesek;
         }
